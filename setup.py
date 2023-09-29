fe749e62bf299b0d69052b3fd880444e3a6c05f7
Natalie Hogg <mail@nataliebhogg.com>
2023-09-29 13:49:21 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
-# NH: all this to be checked (requirements, authorship etc)
 setup(
     name='tdcosmo',
     version='0.1',
     description='TDCOSMO likelihood',
     url="https://github.com/nataliehogg/tdcosmo_ext",
-    author='Cobaya extension by Natalie B. Hogg; original likelihood by Simon Birrer',
+    author='Original likelihood by Simon Birrer; packaged for Cobaya by Natalie B. Hogg',
     python_requires='>=3',
-    install_requires=['hierarc'],
+    install_requires=['hierarc', 'lenstronomy'],
     packages=['tdcosmo'],
     include_package_data=True,
 )
